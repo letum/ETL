c37ce4d21b2f59cb3bbf0de84104bc351ae6c88c
Anton <faitmatveev@gmail.com>
2011-05-21 05:54:52 +0800

    all del

--- transfer/js/main.js
+++
@@ -1,9 +0,0 @@
-function startTransfer() {
-    $('res').innerHTML = '<img src="process.gif" />';
-    var AU = new Ajax.Updater('res', 'trans.php?'+sid,
-        {
-            method: 'post',
-            parameters: $('DB').serialize()+$('info').serialize()
-        }
-    );
-}
